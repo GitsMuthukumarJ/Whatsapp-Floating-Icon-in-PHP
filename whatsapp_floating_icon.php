9513d37e4024d77429fff53ac9ad2622f0681322
Muthukumar J <45478465+GitsMuthukumarJ@users.noreply.github.com>
2022-08-20 10:05:45 +0530

    Create whatsapp_floating_icon.php

---
+++ whatsapp_floating_icon.php
@@ -0,0 +1,27 @@
+<a href="https://api.whatsapp.com/send?text=Hello drgalen.org!&amp;phone=+1 415-251-2044" class="float" target="_blank">
+<i class="fa fa-whatsapp my-float"></i>
+</a>
+
+<style>
+@media only screen and (max-width: 500px) {
+.float{
+	position:fixed;
+	width:60px;
+	height:60px;
+	bottom:40px;
+	right: 20px;
+        top: 50%;
+	background-color:#25d366;
+	color:#FFF;
+	border-radius:50px;
+	text-align:center;
+        font-size:30px;
+	box-shadow: 2px 2px 3px #999;
+        z-index:2;
+}
+
+.my-float{
+	margin-top:16px;
+}
+}
+</style>
